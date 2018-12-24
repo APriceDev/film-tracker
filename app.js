50fc0a1c9a5b1e0e4771639199a00e6bd574916e
apricedev <alanprice.dev1@gmail.com>
2018-12-24 14:56:59 -0500

    installed body-parser

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 const port = 8000;
+
+// body-parser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('film tracker');
