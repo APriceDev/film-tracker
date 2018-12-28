cbecdf3c1bd408617614b8ba5251d7f2b18f4f89
apricedev <alanprice.dev1@gmail.com>
2018-12-28 14:18:48 -0500

    added film schema to models

---
+++ models/Film.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create schema
+const FilmSchema = new Schema({
+  director: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: Number,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  refactor: {
+    type: Boolean,
+    required: true
+  }
+});
+
+mongoose.model('ideas', FilmSchema);
